3b107b4ac7222369c55f0051c5211b45d9e11c85
Norman Kabir <nkabir@gmail.com>
2025-04-13 00:43:12 +0000

    clean up

--- blackbox/dot_bashrc.avail/010-aliases.bash
+++
@@ -1,6 +0,0 @@
-# Aliases
-#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-alias l="ls -lhSt --color"
-alias ll="ls -F --color"
-alias mroe=more
-alias la="ls -lat --color"
---
+++ blackbox/dot_bashrc.avail/100-bash.bash
@@ -0,0 +1,37 @@
+# Aliases
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+function l() {
+  if [ -z "$1" ]; then
+    ls -lhSt --color
+  else
+    ls -lhSt --color "$@"
+  fi
+}
+
+
+function ll() {
+  if [ -z "$1" ]; then
+    ls -lht --color
+  else
+    ls -lht --color "$@"
+  fi
+}
+
+function mroe() {
+  if [ -z "$1" ]; then
+    more
+  else
+    more "$@"
+  fi
+}
+
+
+function la() {
+  if [ -z "$1" ]; then
+    ls -lat --color
+  else
+    ls -lat --color "$@"
+  fi
+}
