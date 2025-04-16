0e525f83765179896a33d99d867fe4aa3d18699c
Norman Kabir <nkabir@gmail.com>
2025-04-16 01:46:35 +0000

    adding host-runtime function

--- blackbox/dot_bashrc.avail/100-bash.bash
+++ blackbox/dot_bashrc.avail/100-bash.bash
@@ -35,3 +35,16 @@ function la() {
     ls -lat --color "$@"
   fi
 }
+
+function host-runtime() {
+    if [ -f /proc/cpuinfo ] && grep -q '^flags.* hypervisor' /proc/cpuinfo; then
+	echo "KERNEL_RUNTIME"
+    elif [ -f /proc/1/environ ] && grep -q 'container=lxc' /proc/1/environ; then
+	echo "OS_RUNTIME"
+    elif [ -f /.dockerenv ] || [ -f /.containerenv ]; then
+	echo "PROCESS_RUNTIME"
+    else
+	# Default case if none of the above are detected
+	echo "KERNEL_RUNTIME"
+    fi
+}
