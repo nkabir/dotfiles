47f715bd542a16c2339b2935c92ac4f505365a3d
Norman Kabir <nkabir@gmail.com>
2025-04-20 18:31:37 +0000

    adding completion to alias

--- blackbox/dot_bashrc.avail/900-chezmoi.bash
+++ blackbox/dot_bashrc.avail/900-chezmoi.bash
@@ -1,6 +1,19 @@
 # ChezMoi
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# function cz() {
-#  chezmoi "$@"
-# }
+alias cz="chezmoi"
+
+# check if __start_chezmoi is defined
+# if not, source the completion script
+if ! type -t __start_chezmoi &>/dev/null; then
+    if [ -f "$HOME/.local/share/bash-completion/completions/chezmoi.bash" ]; then
+	source "$HOME/.local/share/bash-completion/completions/chezmoi.bash"
+	if [[ $(type -t compopt) = "builtin" ]]; then
+	    complete -o default -F __start_chezmoi cz
+	else
+	    complete -o default -o nospace -F __start_chezmoi cz
+	fi
+    else
+	echo "ChezMoi completion script not found."
+    fi
+fi
