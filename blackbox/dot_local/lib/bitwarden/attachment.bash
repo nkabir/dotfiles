785eac80449d7bb6a936cd74c2590fcd9ba517ab
Norman Kabir <nkabir@gmail.com>
2025-05-02 23:06:52 +0000

    adding create attachment

--- blackbox/dot_local/lib/bitwarden/attachment.bash
+++ blackbox/dot_local/lib/bitwarden/attachment.bash
@@ -61,3 +61,39 @@ bitwarden::attachment::id() {
         echo "$attachment_id"
     fi
 }
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+bitwarden::attachment::create() {
+    local note_name="$1"
+    local attachment_path="$2"
+
+    if [[ -z "$note_name" || -z "$attachment_path" ]]; then
+        logger::error "Usage: bitwarden::note::upload-attachment <note_name> <attachment_path>"
+        return 1
+    fi
+
+    if [[ ! -f "$attachment_path" ]]; then
+        logger::error "Attachment file '$attachment_path' does not exist."
+        return 2
+    fi
+
+    # Get the item ID for the note
+    local item_id
+    item_id="$(bitwarden::note::id "$note_name")"
+    if [[ -z "$item_id" ]]; then
+        logger::error "Note named '$note_name' not found."
+        return 4
+    fi
+
+    # Upload the attachment
+    local result
+    result="$(bw create attachment --itemid "$item_id" --file "$attachment_path" 2>&1)"
+    if [[ $? -eq 0 ]]; then
+        logger::info "Successfully uploaded attachment '$attachment_path' to note '$note_name'"
+        return 0
+    else
+        logger::error "Failed to upload attachment: $result"
+        return 5
+    fi
+}
