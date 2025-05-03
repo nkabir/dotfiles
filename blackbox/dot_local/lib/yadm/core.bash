c191cd46a90b0663475420b92e724cb2c3da56ba
Norman Kabir <nkabir@gmail.com>
2025-05-03 09:25:46 +0000

    updating bash

--- blackbox/dot_local/lib/yadm/core.bash
+++ blackbox/dot_local/lib/yadm/core.bash
@@ -7,6 +7,9 @@
 _YADM_CORE=1
 
 YADM_HERE="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+export YADM_GPG_EMAIL="yadm@secrets.github.com"
+export YADM_GPG_NAME="YADM Secrets"
+
 
 . "${YADM_HERE:?}/../logger/core.bash"
 . "${YADM_HERE:?}/../gpg/core.bash"
@@ -14,8 +17,6 @@ YADM_HERE="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && p
 . "${YADM_HERE:?}/bitwarden.bash"
 . "${YADM_HERE:?}/gpg.bash"
 
-YADM_GPG_EMAIL="yadm@secrets.github.com"
-YADM_GPG_NAME="YADM Secrets"
 
 
 # init
@@ -36,88 +37,3 @@ YADM_GPG_NAME="YADM Secrets"
 # encrypt
 
 # decrypt
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-yadm::gpg-id-exists() {
-    local email="${YADM_GPG_EMAIL:?}"
-    local fingerprint
-    fingerprint="$(gpg::get-fingerprint "$email" 2>/dev/null)"
-    if [[ -n "$fingerprint" ]]; then
-        logger::info "GPG identity '$email' exists (fingerprint: $fingerprint)."
-        return 0
-    else
-        logger::warn "GPG identity '$email' does not exist."
-        return 1
-    fi
-}
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-yadm::delete-gpg-key() {
-    local email="${YADM_GPG_EMAIL:?}"
-    local fingerprint
-    fingerprint="$(gpg::get-fingerprint "$email" 2>/dev/null)"
-
-    if [[ -n "$fingerprint" ]]; then
-        logger::info "Deleting GPG key pair for '$email' (fingerprint: $fingerprint)"
-        if gpg::delete-key-pair "$fingerprint"; then
-            logger::info "Successfully deleted GPG key pair"
-        else
-            logger::error "Failed to delete GPG key pair"
-            return 1
-        fi
-    else
-        logger::warn "No GPG key pair found for '$email'"
-    fi
-}
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-yadm::export-gpg-keypair() {
-    local email="${YADM_GPG_EMAIL:?}"
-    local datetime
-    datetime="$(date '+%Y%m%d-%H%M%S')"
-    local pubfile="gpg-public-${email//[^a-zA-Z0-9]/_}-$datetime.asc"
-    local privfile="gpg-private-${email//[^a-zA-Z0-9]/_}-$datetime.asc"
-
-    if ! yadm::gpg-id-exists; then
-        logger::error "No GPG key found for '$email'. Aborting export."
-        return 1
-    fi
-
-    logger::info "Exporting public key for '$email' to '$pubfile'"
-    if gpg::export-public-key "$email" > "$pubfile"; then
-        logger::info "Public key exported to $pubfile"
-    else
-        logger::error "Failed to export public key for '$email'"
-        return 1
-    fi
-
-    logger::info "Exporting private key for '$email' to '$privfile'"
-    if gpg::export-private-key "$email" > "$privfile"; then
-        logger::info "Private key exported to $privfile"
-    else
-        logger::error "Failed to export private key for '$email'"
-        return 1
-    fi
-
-    logger::warn "Keep your private key file ($privfile) secure!"
-}
-
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-yadm::create-gpg-keypair() {
-    local name="${YADM_GPG_NAME:?}"
-    local email="${YADM_GPG_EMAIL:?}"
-
-    if yadm::gpg-id-exists; then
-        logger::warn "GPG key for '$email' already exists. Aborting key creation."
-        return 1
-    fi
-
-    logger::info "Creating new GPG key pair for '$name '"
-    if gpg::create-key-pair "$name" "$email"; then
-        logger::info "Successfully created GPG key pair for '$email'"
-    else
-        logger::error "Failed to create GPG key pair for '$email'"
-        return 1
-    fi
-}
--- blackbox/dot_local/lib/yadm/gpg.bash
+++ blackbox/dot_local/lib/yadm/gpg.bash
@@ -9,6 +9,28 @@ _YADM_GPG=1
 # yadm::gpg::init
 # create a new keypair in gpg with email yadm@secrets.github.com if it does not exist
 
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+yadm::gpg::init() {
+
+    local gpg_id
+    gpg_id=$(gpg::id "$YADM_GPG_EMAIL")
+
+    if [[ -n $gpg_id ]]; then
+        logger::info "GPG key for $YADM_GPG_EMAIL already exists"
+        return 0
+    fi
+
+    logger::info "Creating new GPG keypair: $YADM_GPG_NAME <$YADM_GPG_EMAIL>"
+    if gpg::create "$YADM_GPG_NAME" "$YADM_GPG_EMAIL"; then
+        logger::info "Successfully created GPG keypair for YADM"
+        return 0
+    else
+        logger::error "Failed to create GPG keypair"
+        return 1
+    fi
+}
+
+
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # yadm::gpg::backup
