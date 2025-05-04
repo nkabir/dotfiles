74e4e31a82ae5c318751d697f3cad744ce7e93aa
Norman Kabir <nkabir@gmail.com>
2025-05-04 16:00:50 +0000

    updating bitwarden/account.bash

--- blackbox/dot_local/lib/bitwarden/account.bash
+++ blackbox/dot_local/lib/bitwarden/account.bash
@@ -9,8 +9,17 @@ _BITWARDEN_ACCOUNT=1
 
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Returns the Bitwarden session ID or nothing
+bitwarden::account::session() {
+
+    echo $BW_SESSION
+}
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Returns true if session is unlocked
 bitwarden::account::is-unlocked() {
-    if [[ -z "${BW_SESSION}" ]]; then
+    if [[ -z "$(bitwarden::account::session)" ]]; then
 
 	logger::warn "Bitwarden session is not unlocked"
 	return 1
@@ -18,9 +27,24 @@ bitwarden::account::is-unlocked() {
     return 0
 }
 
+
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Synchronizes the Bitwarden account
 bitwarden::account::sync() {
 
     logger::info "Syncing Bitwarden account"
     bw sync
+}
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+bitwarden::account::list-folders() {
+    logger::info "Listing all Bitwarden folder names"
+    local folders
+    folders=$(bw list folders 2>/dev/null | jq -r '.[].name')
+    if [[ -z "$folders" ]]; then
+        logger::info "No folders found in Bitwarden vault."
+        return 0
+    fi
+    echo "$folders"
 }
