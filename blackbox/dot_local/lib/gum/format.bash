93f2d05923bc72f0455570cb1b7bda60516a85d3
Norman Kabir <nkabir@gmail.com>
2025-05-15 22:54:22 +0000

    implementing gum::format

--- blackbox/dot_local/lib/gum/format.bash
+++ blackbox/dot_local/lib/gum/format.bash
@@ -2,8 +2,39 @@
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# gum::format [--type <type>] [TEMPLATE...]
+# Arguments correspond to "gum format" CLI flags and templates.
 gum::format() {
+    # shellcheck disable=SC2154
+    local args=()
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --type|-t)
+                args+=(--type "$2")
+                shift 2
+                ;;
+            --*)
+                # Unknown flag, pass through
+                args+=("$1")
+                shift
+                ;;
+            *)
+                # Positional arguments (template strings)
+                args+=("$1")
+                shift
+                ;;
+        esac
+    done
 
-    return 0
+    # shellcheck disable=SC1091
+    . "${GUM_HERE:?}/../logger/core.bash"
+
+    if ! command -v gum >/dev/null 2>&1; then
+        logger::error "gum CLI not found in PATH"
+        return 2
+    fi
+
+    gum format "${args[@]}"
 }
 export -f gum::format
