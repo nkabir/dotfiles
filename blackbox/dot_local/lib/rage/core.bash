606fc792bbc81c8cd23b4bf48fdcde2ce907561f
Norman Kabir <nkabir@gmail.com>
2025-05-17 20:28:44 +0000

    adding rage

--- blackbox/dot_local/lib/rage/core.bash
+++ blackbox/dot_local/lib/rage/core.bash
@@ -6,3 +6,9 @@ _RAGE_CORE=1
 
 RAGE_HERE="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" \
     &> /dev/null && pwd 2> /dev/null; )";
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Load the rage functions
+. "${RAGE_HERE:?}/../logger/core.bash"
+. "${RAGE_HERE:?}/encrypt.bash"
