bca9fc11c72c8708b8fa1be5f204c91438e6f340
Norman Kabir <nkabir@gmail.com>
2025-05-17 20:43:50 +0000

    updating login.bash

--- blackbox/dot_local/lib/bitwarden/login.bash
+++ blackbox/dot_local/lib/bitwarden/login.bash
@@ -1,18 +1,3 @@
 # bitwarden/login.bash
-
-bitwarden::login::client-id() {
-
-    local client_id="${1}"
-    if [[ -z "${client_id}" ]]; then
-	client_id="$(skate get "bitwarden/client_id")"
-	if [[ -z "${client_id}" ]]; then
-	    echo "No client ID found. Please set it using skate."
-	    return 1
-	fi
-    elif [[ "${client_id}" == "none" ]]; then
-	if ! skate set "bitwarden/client_id" "${client_id}"; then
-	    echo "Failed to set client ID."
-	    return 1
-	fi
-    fi
-}
+# shellcheck shell=bash
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::
