4c80a0a3838fe861a9b8c44a398885684933dc5e
Norman Kabir <nkabir@gmail.com>
2025-05-21 08:28:43 +0000

    updating emacs

--- blackbox/dot_local/bin/executable_emacs-dev.sh
+++ blackbox/dot_local/bin/executable_emacs-dev.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-$HOME/realms/rkx/src/polylith/emacs/development/emacs-dev.sh
+# Override when needed
+export POLYLITH_EMACS=$HOME/realms/rkx/src/polylith/emacs
+
+HERE="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" \
+    &> /dev/null && pwd 2> /dev/null; )";
+
+emacs --debug-init --init-directory "$POLYLITH_EMACS/development" --no-splash $@
+
+
