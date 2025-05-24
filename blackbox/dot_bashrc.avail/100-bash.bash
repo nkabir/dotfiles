564c65a83e75853e7e9d0f8b31908539337c5741
Norman Kabir <nkabir@gmail.com>
2025-05-24 20:06:03 +0000

    adding rust

--- blackbox/dot_bashrc.avail/100-bash.bash
+++ blackbox/dot_bashrc.avail/100-bash.bash
@@ -50,4 +50,16 @@ function host-runtime() {
     fi
 }
 
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_RUNTIME_DIR=$HOME/.local/run
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_PICTURES_DIR=$HOME/Pictures
+export XDG_VIDEOS_DIR=$HOME/Videos
+export XDG_MUSIC_DIR=$HOME/Music
+export XDG_DOCUMENTS_DIR=$HOME/Documents
+export XDG_DOWNLOAD_DIR=$HOME/Downloads
+export XDG_DESKTOP_DIR=$HOME/Desktop
+
 export HOST_RUNTIME=$(host-runtime)
